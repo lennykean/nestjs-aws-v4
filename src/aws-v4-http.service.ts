e4ef33fec535fd3ecf009f5f59f9325aab5530a9
Kyle Webb <kywebb@lumeris.com>
2023-07-12 15:15:00 -0500

    fix typo

--- src/aws-v4-http.service.ts
+++ src/aws-v4-http.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common';
 import { AXIOS_INSTANCE_TOKEN } from '@nestjs/common/http/http.constants';
 import { aws4Interceptor } from 'aws4-axios/dist/interceptor';
-import type { AxiosInstance } from 'axios'
+import { AxiosInstance } from 'axios'
 import { HttpService } from '@nestjs/axios';
 import { AwsV4HttpModuleOptions } from './aws-v4-http.module';
 import { AWS_V4_HTTP_MODULE_OPTIONS } from './constants';
